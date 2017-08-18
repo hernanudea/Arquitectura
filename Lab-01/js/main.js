52f2c183c69ea2e6078697082690baf789429463
hernanudea <lcallf21>
2017-08-17 22:43:59 -0500

    Se crea función para conversión de de parte decinal de numero en base 10 a base n.

--- Lab-01/js/main.js
+++ Lab-01/js/main.js
@@ -1,9 +1,14 @@
+function concatenador10aN(base10, baseN) {
+    var res = base10.toString().split(".");
+    var entero = convertirEntero10aN(res[0], baseN);
+    var decimal = convertirDecimal10aN("0." + res[1], baseN);
+    return entero + decimal;
+}
 
-
-function convertir10aN(base10, baseN) {
+function convertirEntero10aN(base10, baseN) {
     var respuesta = "";
     var base10Modificada = base10;
-    while (base10Modificada >= baseN){
+    while (base10Modificada >= baseN) {
         respuesta = (base10Modificada % baseN) + respuesta;
         base10Modificada = Math.floor(base10Modificada / baseN);
     }
@@ -11,14 +16,29 @@ function convertir10aN(base10, baseN) {
     return respuesta;
 }
 
-(function () {
-    console.log("La respuesta es: " + convertir10aN(54545, 7));
-    // console.log("La respuesta es: " + 7/2);
-}());
+function convertirDecimal10aN(base10, baseN) {
+    var respuesta = "";
+    var ultimoDecinal = base10;
+
+    do {
+        var producto = ultimoDecinal * baseN;
+        var arregloProducto = producto.toString().split(".")
+        respuesta += arregloProducto[0];
+        ultimoDecinal = "0." + arregloProducto[1];
+    } while (respuesta.length < 10);
+    if (respuesta.length === 0) {
+        return "";
+    } else {
+        return "." + respuesta;
+    }
+}
+
 
 /**
- * base10 =               7
- * baseN =                2
- * respuesta =            1
- * base10Modificada =     7,
+ * función anonima temporal de ejecución
  */
+(function () {
+    var num10 = 1000.51832;
+    var base = 7;
+    document.getElementById("resultado").innerHTML = "El numero " + num10 + " en base " + base + " es: " + concatenador10aN(num10, base);
+}());
