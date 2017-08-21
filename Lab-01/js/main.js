e620a9ad202da9061a61171c400099a52ed5400e
hernan <hernanudea@gmail.com>
2017-08-21 16:48:08 -0500

    Se mejora metodos del js y la vista en HTML

--- Lab-01/js/main.js
+++ Lab-01/js/main.js
@@ -1,7 +1,12 @@
 function concatenar10aN(numBase10, baseN) {
+
     var arrayNum = numBase10.toString().split(".");
     var entero = convertirEntero10aN(arrayNum[0], baseN);
-    var decimal = convertirDecimal10aN("0." + arrayNum[1], baseN);
+    if (isNaN(arrayNum[1])) {
+        decimal = "";
+    } else {
+        var decimal = convertirDecimal10aN("0." + arrayNum[1], baseN);
+    }
     return entero + decimal;
 }
 
@@ -17,14 +22,19 @@ function convertirEntero10aN(numBase10, baseN) {
 }
 
 function convertirDecimal10aN(numBase10, baseN) {
+    var arregloDecinales = new Array;
     var respuesta = "";
     var ultimoDecimal = numBase10;
-
     do {
         var producto = ultimoDecimal * baseN;
         var arregloProducto = producto.toString().split(".");
-        respuesta += arregloProducto[0];
         ultimoDecimal = "0." + arregloProducto[1];
+        if (arregloDecinales.indexOf(ultimoDecimal) === (-1)) {
+            respuesta += arregloProducto[0];
+            arregloDecinales[respuesta.length] = ultimoDecimal;
+        } else {
+            return "." + respuesta;
+        }
     } while (respuesta.length < 10);
     if (respuesta.length === 0) {
         return "";
@@ -36,7 +46,11 @@ function convertirDecimal10aN(numBase10, baseN) {
 function concatenarNa10(numero, baseN) {
     var arregloNum = numero.toString().split(".");
     var entero = convertirEnteroNa10(arregloNum[0], baseN);
-    var decimal = convertirDecimalNa10("0." + arregloNum[1], baseN);
+    if (isNaN(arregloNum[1])) {
+        decimal = "";
+    } else {
+        var decimal = convertirDecimalNa10("0." + arregloNum[1], baseN);
+    }
     return entero + decimal;
 }
 
@@ -58,10 +72,17 @@ function convertirDecimalNa10(numero, baseN) {
     return respuesta;
 }
 
+function esNumero(num) {
+    if (isNaN(num)) {
+        alert("No es un numero valido");
+        document.getElementById("numero").value = "";
+    }
+}
+
 (function () {
-    var num = 8524.45;
+    var num = 8524.4;
     var base = 7;
-    var numN = 33565.3102310231;
+    var numN = 4567;
 
     document.getElementById("base7").innerHTML = concatenar10aN(num, base);
     document.getElementById("base10").innerHTML = concatenarNa10(numN, base);
